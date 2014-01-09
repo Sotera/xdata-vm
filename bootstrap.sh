44d140e2a4fad46e2f0676a342b581c243f9d1d5
Scott Haleen <scott.haleen@gmail.com>
2014-01-09 14:24:17 -0500

    added .gitignore added static ipaddress removed grub-pc update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y remove grub-pc
-sudo apt-get -y install grub-pc
-sudo grub-install /dev/sda # precaution
-sudo update-grub 
+
+#https://github.com/mitchellh/vagrant/issues/289
+#sudo apt-get -y remove grub-pc
+#sudo apt-get -y install grub-pc
+#sudo grub-install /dev/sda # precaution
+#sudo update-grub 
+
+#sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+
+
 # now start bootstrapping and upgrading your system packages
 sudo apt-get -y upgrade
 sudo apt-get -y update
 sudo useradd -m -s /bin/bash bigdata
-echo -e 'bigdata\nbigdata' | sudo passwd bigdata
+printf '%s\n%s\n' 'bigdata' 'bigdata' | sudo passwd bigdata
 sudo usermod -aG sudo bigdata
-
-
 
 #sudo git clone git://github.com/Kitware/tangelo.git
 #sudo mkdir tangelo-build
